a5b62d4a735dd78912941a13a66933bbdb246811
Emelyne12 <ingabiremelyne1234@gmail.com>
2026-03-05 09:30:54 +0200

    IDs

--- id-card-generator/src/data/people.js
+++ id-card-generator/src/data/people.js
@@ -1,20 +1,20 @@
 const people = [
   {
-    id: 1,
+    id: 0211,
     name: "Alice Johnson",
     role: "web deveroping",
     department: "Web Developer",
     avatar: "https://api.dicebear.com/9.x/adventurer/svg?seed=Alice",
   },
   {
-    id: 2,
+    id: 0223,
     name: "Kevin Underson",
     role: "UI/UX Designer",
     department: "Web Designer",
     avatar: "https://api.dicebear.com/9.x/adventurer/svg?seed=Michael",
   },
   {
-    id: 3,
+    id: 0234,
     name: "Chanelle Maya",
     role: "Information Manager",
     department: "Information Management",
